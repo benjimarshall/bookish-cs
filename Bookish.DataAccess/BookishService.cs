4c197425d7a1c2a92cc9354cacea22c19b15ea3e
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-23 14:49:39 +0100

    Added loan queries to SQL service

--- Bookish.DataAccess/BookishService.cs
+++ Bookish.DataAccess/BookishService.cs
@@ -21,5 +21,29 @@ namespace Bookish.DataAccess
 
             return connection.Query<Book>(sqlString);
         }
+
+        public User GetUser(string name)
+        {
+            var sqlString = $"SELECT id FROM users WHERE username='{name}'";
+            return connection.QueryFirstOrDefault<User>(sqlString);
+        }
+
+        public IEnumerable<JoinedLoan> GetJoinedLoans(string username)
+        {
+            var user = GetUser(username);
+
+            var sqlString =
+                @"SELECT loans.due AS DueDate,
+                       books.title as Title,
+	                   books.Authors as Authors,
+	                   bookcopies.isbn AS ISBN,
+                       bookcopies.id AS CopyId
+                FROM loans
+                INNER JOIN bookcopies ON loans.bookid = bookcopies.id
+                INNER JOIN books ON books.isbn = bookcopies.isbn
+                WHERE loans.userid = '" + user?.Id + "';";
+
+            return connection.Query<JoinedLoan>(sqlString);
+        }
     }
 }
---
+++ Bookish.DataAccess/Records/JoinedLoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookish.DataAccess.Records
+{
+    public class JoinedLoan
+    {
+        public DateTime DueDate { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Isbn { get; set; }
+        public int CopyId { get; set; }
+    }
+}
