8ad69edbe1507096a371b716fa2795a7352d3423
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-23 09:33:16 +0100

    Tidying up initial insertion query

--- SqlQueries/InsertData.sql
+++ SqlQueries/InsertData.sql
@@ -1,22 +1,30 @@
-INSERT INTO users(username) VALUES (N'Benji');
-INSERT INTO users(username) VALUES (N'Frank');
-INSERT INTO users(username) VALUES (N'Frida');
-INSERT INTO users(username) VALUES (N'Shiv');
+SET IDENTITY_INSERT users ON;
+INSERT INTO users(id, username) 
+VALUES (1, 'Benji'), 
+       (2, 'Frank'), 
+	   (3, 'Frida'), 
+	   (4, 'Shiv');
+SET IDENTITY_INSERT users OFF;
 
-INSERT INTO books(isbn, title, authors) VALUES (N'978-0-12-383872-8', N'Computer Architecture: A Quantitative Approach', N'Hennessy, Patterson');
-INSERT INTO books(isbn, title, authors) VALUES (N'978-0-12-385060-7', N'Computer Networks: A Systems Approach', N'Peterson, Davie');
-INSERT INTO books(isbn, title, authors) VALUES (N'978-0-13-300214-0', N'Compilers: Principles, Techniques, and Tools', N'Aho, Lam, Ullman, Sethi');
-INSERT INTO books(isbn, title, authors) VALUES (N'978-0-262-03293-3', N'Introduction To Algorithms', N'Cormen, Leiserson, Rivest, Stein');
+INSERT INTO books(isbn, title, authors)
+VALUES ('978-0-12-383872-8', 'Computer Architecture: A Quantitative Approach', 'Hennessy, Patterson'),
+	   ('978-0-12-385060-7', 'Computer Networks: A Systems Approach', 'Peterson, Davie'),
+	   ('978-0-13-300214-0', 'Compilers: Principles, Techniques, and Tools', 'Aho, Lam, Ullman, Sethi'),
+	   ('978-0-262-03293-3', 'Introduction To Algorithms', 'Cormen, Leiserson, Rivest, Stein');
 
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-12-383872-8');
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-12-383872-8');
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-12-383872-8');
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-12-385060-7');
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-262-03293-3');
-INSERT INTO bookcopies(isbn) VALUES (N'978-0-262-03293-3');
+SET IDENTITY_INSERT bookcopies ON;
+INSERT INTO bookcopies(id, isbn)
+VALUES (1, '978-0-12-383872-8'), 
+	   (2, '978-0-12-383872-8'), 
+	   (3, '978-0-12-383872-8'), 
+	   (4, '978-0-12-385060-7'), 
+	   (5, '978-0-262-03293-3'), 
+	   (6, '978-0-262-03293-3');
+SET IDENTITY_INSERT bookcopies OFF;
 
-INSERT INTO loans(bookid, userid, due) VALUES (1, 1, '2020-07-15');
-INSERT INTO loans(bookid, userid, due) VALUES (5, 1, '2020-11-07');
-INSERT INTO loans(bookid, userid, due) VALUES (2, 2, '2021-04-01');
-INSERT INTO loans(bookid, userid, due) VALUES (6, 4, '2020-10-25');
+INSERT INTO loans(bookid, userid, due) 
+VALUES (1, 1, '2020-07-15'),
+       (5, 1, '2020-11-07'),
+	   (2, 2, '2021-04-01'),
+	   (6, 4, '2020-10-25');
 
